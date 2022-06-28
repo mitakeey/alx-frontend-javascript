21d47118ed64f7a4835b4e3ca747836067212983
mitakeey <mitakeeykiage@gmail.com>
2022-06-28 18:21:07 +0300

    :art: es6 classes

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,17 @@
 export default class Building {
-    constructor(sqft) {
-      if (typeof sqft !== 'number') {
-        throw new TypeError('Sqft must be a number')
-      }
-  
-      this._sqft = sqft
+  constructor(sqft) {
+    if (
+      new.target !== Building && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw new Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
     }
-  
-    get sqft() {
-      return this._sqft
-    }
-  
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage')
-    }
+    this._sqft = sqft
   }
+
+  // sqft getter
+  get sqft() {
+    return this._sqft
+  }
+}
