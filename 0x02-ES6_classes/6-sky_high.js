b69336691adedff19c52302359afb4efdc343cce
mitakeey <mitakeeykiage@gmail.com>
2022-06-28 18:22:59 +0300

    :art: es6 clases

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,27 +1,19 @@
-import Building from './5-building'
+
+
+import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number')
-    } else if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number')
-    }
-
-    super(sqft)
-    this._sqft = sqft
-    this._floors = floors
+    super(sqft);
+    this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft
-  }
-
+  // floors getter
   get floors() {
-    return this._floors
+    return this._floors;
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
